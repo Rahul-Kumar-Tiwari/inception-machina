b915dfae84928c480ec5822f028f704fc98b92f8
Rahul Kumar Tiwari <31995292+Rahul-Kumar-Tiwari@users.noreply.github.com>
2018-10-24 13:54:21 +0530

    Updated handling.py
    Made some changes in the code.

--- handling.py
+++ handling.py
@@ -1,23 +1,23 @@
 try:
     a = 9
-    #b = a/0                             #Zero division error
+    b = a/0                             #Zero division error
     f = open("abc.txt")                 #File not found
     for line in f:
         print(line)
 
-# except FileNotFoundError:
-#     print("File not find")
-#
-# except ZeroDivisionError:
-#     print("Divided by zero you moron")
+except FileNotFoundError:
+    print("File not find")
 
-# except (FileNotFoundError, ZeroDivisionError):        #combined excetion
-#      print("error caught")
+except ZeroDivisionError:
+    print("Divided by zero you moron")
 
-# except Exception as e:                      #accepts all kinds of exception
-#     print("Error : ",e)
+except (FileNotFoundError, ZeroDivisionError):        #combined excetion
+     print("error caught")
 
-except FileNotFoundError as e:
-    print(e.filename)
+except Exception as e:                      #accepts all kinds of exception
+    print("Error : ",e)
+
+# except FileNotFoundError as e:
+#     print(e.filename)
     
 # We added it here just for fun
